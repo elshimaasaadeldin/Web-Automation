d9b3dd677779f3b1755502b677507811417aa3c5
esaad <esaad@webalo.net>
2026-01-22 10:36:03 +0200

    commit - Update comments

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -5,29 +5,28 @@ import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
 
-	
 	// WebDriver instance
 	private WebDriver driver;
 
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
 	}
-	
+
 	// Login Page locators
-	
+
 	private By emailInput = By.name("email");
 	private By passwordInput = By.name("password");
 	private By loginBtn = By.xpath("//button[text()='Login']");
 	private By loggedInUser = By.xpath("//a[contains(text(),'Logged in as')]");
 
+	// Login Page actions
 	public void login(String email, String password) {
 		driver.findElement(emailInput).sendKeys(email);
 		driver.findElement(passwordInput).sendKeys(password);
 		driver.findElement(loginBtn).click();
 	}
-	
-	// Login Page actions
 
+//Assertion
 	public boolean isUserLoggedIn() {
 		return driver.findElement(loggedInUser).isDisplayed();
 	}
